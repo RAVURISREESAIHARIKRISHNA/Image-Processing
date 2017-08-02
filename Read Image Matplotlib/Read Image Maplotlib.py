3be0824351bac0b822b01f843989d5eec6323f2b
RAVURI SREE SAI HARI KRISHNA <RAVURISREESAIHARIKRISHNA@users.noreply.github.com>
2017-08-02 23:35:08 +0530

    Create Read Image Maplotlib.py

---
+++ Read Image Matplotlib/Read Image Maplotlib.py
@@ -0,0 +1,8 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import matplotlib.image as mpig
+my_img = cv2.imread("G:\\Python DB\\images\\face sample10.jpg" )
+plt.imshow(my_img )
+plt.xticks([]) , plt.yticks([])
+plt.show()
